ae093364b0e71941afb1aee278c1fb21112e3d73
antonioofdz <amfernandez@topdoctors.es>
2022-03-30 23:30:32 +0200

    Lee el contrato inteligente

--- backend/main.go
+++ backend/main.go
@@ -1,17 +1,30 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+
+	"code.ual/backend/internal/contracts"
+)
+
 func main() {
-	// client, err := ethclient.Dial("https://rinkeby.infura.io")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	client, err := ethclient.Dial("http://127.0.0.1:8545")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// address := common.HexToAddress("0x147B8eb97fD247D06C4006D269c90C1908Fb5D54")
-	// instance, err := contracts.NewStore(address, client)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	address := common.HexToAddress("0x411A42CbeaE8F101508eAB8afE2D5c2d7C6e564f")
+	caller, err := contracts.NewContractCaller(address, client)
+	if err != nil {
+		log.Fatal(err)
+	}
+	resp, err := caller.HelloWorld(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// fmt.Println("contract is loaded")
-	// _ = instance
+	fmt.Println("contract is loaded", resp)
 }
--- go.mod
+++ go.mod
@@ -13,11 +13,13 @@ require (
 	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/google/uuid v1.2.0 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/mattn/go-colorable v0.1.11 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/rjeczalik/notify v0.9.1 // indirect
 	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
 	github.com/tklauser/go-sysconf v0.3.5 // indirect
 	github.com/tklauser/numcpus v0.2.2 // indirect
-	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2 // indirect
-	golang.org/x/sys v0.0.0-20210816183151-1e6c022a8912 // indirect
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	golang.org/x/sys v0.0.0-20211103235746-7861aae1554b // indirect
 	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce // indirect
 )
